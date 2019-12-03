4b501c30a372f333898e0cde830cd72cb17520cb
Nathan95 <nathan@mediazoo.tv>
2019-12-03 22:12:23 +0000

    register works nicely

---
+++ Sagat/playground/axios.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+
+let config = {
+	headers: { 'Content-Type': 'application/json' }
+};
+
+let data = {
+	username: 'tyrone',
+	firstName: 'austin',
+	lastName: 'marks',
+	password: 'austin123',
+	passwordConfirm: 'austin123',
+	email: 'austin@live.com'
+};
+
+axios
+	.post('http://localhost:3000/users', data)
+	.then(response => {
+		console.log('successful');
+	})
+	.catch(err => {
+		console.log(err);
+		console.log('already exists');
+	});
+
+/*axios
+	.post('http://localhost:3090/signin', data)
+	.then(response => {
+		console.log(response.data.token);
+	})
+	.catch(err => {
+		console.log('user details incorrect');
+	});
+
+/*axios
+	.get('http://localhost:3090/private/private', {
+		headers: {
+			'x-auth':
+				'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ZDdhOWUyNGIxOGViNTY3YWM5MDhlMDkiLCJpYXQiOjE1NjgzMjYyODM4NTl9.cybYLTmdMliyreqITc79ysEw20D-PFLEUkOHA83PCAY'
+		}
+	})
+	.then(response => {
+		console.log(response.data);
+	})
+	.catch(err => {
+		console.log(err);
+	});*/
